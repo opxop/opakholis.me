9d57f0b02d263059bb97985a4bfd8a4a3f52b3b1
Opa Kholis Majid <opakholis.id@gmail.com>
2020-11-01 11:40:43 +0700

    :boom: component : add timeline section

---
+++ components/Timeline.js
@@ -0,0 +1,101 @@
+import React from "react";
+import {
+  Divider,
+  Flex,
+  Heading,
+  Icon,
+  List,
+  Stack,
+  Text,
+  ListItem,
+  useColorMode,
+  Link,
+} from "@chakra-ui/core";
+
+const YearDivider = () => {
+  const { colorMode } = useColorMode();
+  const borderColor = {
+    light: "gray.200",
+    dark: "gray.600",
+  };
+
+  return <Divider borderColor={borderColor[colorMode]} my={8} w="100%" />;
+};
+
+const TimelineStep = ({ title, children }) => {
+  const { colorMode } = useColorMode();
+  const color = {
+    light: "gray.700",
+    dark: "gray.400",
+  };
+
+  return (
+    <ListItem>
+      <Stack ml={2} mb={4}>
+        <Flex align="center">
+          <Icon name="check-circle" mr={2} color="whatsapp.500" />
+          <Text fontWeight="medium">{title}</Text>
+        </Flex>
+        <Text color={color[colorMode]} ml={6}>
+          {children}
+        </Text>
+      </Stack>
+    </ListItem>
+  );
+};
+
+const Timeline = () => {
+  return (
+    <Flex
+      flexDirection="column"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      maxWidth="700px"
+      mt={8}
+    >
+      <Heading letterSpacing="tight" mb={4} size="xl" fontWeight="bold">
+        Timeline
+      </Heading>
+      <Heading
+        as="h3"
+        size="lg"
+        fontWeight="bold"
+        mb={4}
+        letterSpacing="lighter"
+      >
+        2020
+      </Heading>
+      <List>
+        <TimelineStep title="Landed First Internship 👨🏼‍💻">
+          Merupakan 3 bulan yang sangat berarti. Bersama orang-orang hebat di{" "}
+          <Link href="http://jiwalu.id" isExternal>
+            Jiwalu Studio
+          </Link>{" "}
+          saya banyak belajar hal baru.
+        </TimelineStep>
+      </List>
+      <YearDivider />
+      <Heading
+        as="h3"
+        size="lg"
+        fontWeight="bold"
+        mb={4}
+        letterSpacing="lighter"
+      >
+        2019
+      </Heading>
+      <List>
+        <TimelineStep title="3rd Place at Diskominfo Karawang Competition 🎖">
+          Berkolaborasi dengan tim sebagai Front-end developer. Menciptakan
+          sebuah inovasi baru dengan membangun sebuah sistem berbasis web dengan
+          tema "smart city".
+        </TimelineStep>
+        <TimelineStep title="Got BNSP Certified 🎉">
+          Sebagai Web Pemrograman
+        </TimelineStep>
+      </List>
+    </Flex>
+  );
+};
+
+export default Timeline;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,45 @@
 import React from "react";
-import { useColorMode } from "@chakra-ui/core";
+import { Heading, Flex, Text, Box, Stack, useColorMode } from "@chakra-ui/core";
 
 import Container from "../components/Container";
+import Timeline from "../components/Timeline";
 
 const Index = () => {
-  return <Container>Hallo World</Container>;
+  const { colorMode } = useColorMode();
+  const secondaryTextColor = {
+    light: "gray.700",
+    dark: "gray.400",
+  };
+
+  return (
+    <Container>
+      <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxW="700px"
+      >
+        <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+        >
+          <Heading letterSpacing="tight" mt={5} mb={4} as="h1" size="2xl">
+            Halo, Saya Opa Kholis Majid
+          </Heading>
+          <Text color={secondaryTextColor[colorMode]}>
+            Seseorang yang meyebut dirinya sebagai Front-end developer, yang
+            mana sekarang sedang senang ber-ekplorasi dengan Linux distribution
+            dan Open-source Software.
+          </Text>
+        </Flex>
+        <Timeline />
+      </Stack>
+    </Container>
+  );
 };
 
 export default Index;
