37823a2de51e7155c623d2a1865a89909e662ad8
Opa Kholis Majid <opakholis.id@gmail.com>
2020-11-01 20:26:06 +0700

    :sparkles: component : set to show up projects

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import React from "react";
 import Container from "../components/Container";
 import Timeline from "../components/Timeline";
 import Landing from "../components/Landing";
+import Project from "../components/Project";
 
 const Index = () => {
   return (
     <Container>
       <Landing />
+      <Project />
       <Timeline />
     </Container>
   );
