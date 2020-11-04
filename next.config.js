3ed5963c42183206a2b22d035d59a2996d6cc0e7
Opa Kholis Majid <opakholis.id@gmail.com>
2020-11-04 15:30:37 +0700

    :wrench: remark config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 const readingTime = require('reading-time');
 const withMdxEnhanced = require('next-mdx-enhanced');
+const mdxPrism = require('mdx-prism');
 
 module.exports = withMdxEnhanced({
   layoutPath: 'layouts',
   defaultLayout: true,
+  remarkPlugins: [
+    require('remark-code-titles'),
+    require('remark-autolink-headings'),
+    require('remark-slug')
+  ],
+  rehypePlugins: [mdxPrism],
   extendFrontMatter: {
     process: (mdxContent) => ({
       wordCount: mdxContent.split(/\s+/gu).length,
