74f27e7c10ace4b6ed0a1af6fa73467a72827359
Opa Kholis Majid <opakholis.id@gmail.com>
2020-11-07 10:48:53 +0700

    :bug: fix typo

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -11,25 +11,25 @@ const prettier = require('prettier');
   ]);
 
   const sitemap = `
-  <?xml version=1.0 encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${pages
-    .map((page) => {
-      const path = page
-        .replace('pages', '')
-        .replace('.js', '')
-        .replace('.mdx', '');
-      const route = path === '/index' ? '' : path;
+  <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages
+        .map((page) => {
+          const path = page
+            .replace('pages', '')
+            .replace('.js', '')
+            .replace('.mdx', '');
+          const route = path === '/index' ? '' : path;
 
-      return `
-    <url>
-      <loc>${`https://opakholis.me${route}`}</loc>
-    </url>
-    `;
-    })
-    .join('')}
-  </urlset>
-`;
+          return `
+            <url>
+              <loc>${`https://opakholis.me${route}`}</loc>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>
+  `;
 
   const formatted = prettier.format(sitemap, {
     ...prettierConfig,
