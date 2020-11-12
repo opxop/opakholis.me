a65d5dd13a46a3d1c344263e6fd6df81ac349995
Opa Kholis Majid <opakholis.id@gmail.com>
2020-11-12 15:21:09 +0700

    :sparkles: add tag, category, and search by topics

--- layouts/index.js
+++ layouts/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { parseISO, format } from 'date-fns';
-import { useColorMode, Heading, Text, Flex, Icon } from '@chakra-ui/core';
+import {
+  useColorMode,
+  Heading,
+  Text,
+  Flex,
+  Icon,
+  Box,
+  Badge
+} from '@chakra-ui/core';
 
 import BlogSeo from '../components/BlogSeo';
 import Container from '../components/Container';
@@ -26,7 +34,7 @@ export default function BlogLayout({ children, frontMatter }) {
         maxWidth="700px"
         w="100%"
       >
-        <Heading letterSpacing="tight" mb={2} as="h1" size="2xl">
+        <Heading letterSpacing="tight" mb={1} as="h1" size="2xl">
           {frontMatter.title}
         </Heading>
         <Text fontSize="sm" color={secondaryTextColor[colorMode]}>
@@ -37,6 +45,26 @@ export default function BlogLayout({ children, frontMatter }) {
           {frontMatter.readingTime.text}
         </Text>
       </Flex>
+      {frontMatter.tags && (
+        <Box mt={-4}>
+          {frontMatter.tags.map((topic, i) => {
+            return (
+              <Badge
+                key={i}
+                rounded="md"
+                px={2}
+                py={1}
+                mr={2}
+                fontWeight="normal"
+                textTransform="none"
+                variantColor="gray"
+              >
+                {topic}
+              </Badge>
+            );
+          })}
+        </Box>
+      )}
       {children}
     </Container>
   );
--- pages/blog.js
+++ pages/blog.js
@@ -8,7 +8,8 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Icon
+  Icon,
+  PseudoBox
 } from '@chakra-ui/core';
 
 import Container from '../components/Container';
@@ -21,22 +22,55 @@ const title = 'Blog - Opa Kholis Majid';
 const description = 'Thoughts on the programming, tech, and my personal life.';
 
 const Blog = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [selectedTopics, setSelectedTopics] = useState([]);
+
   const { colorMode } = useColorMode();
   const secondaryTextColor = {
     light: 'gray.700',
     dark: 'gray.400'
   };
 
-  const [searchValue, setSearchValue] = useState('');
+  const topicsBg = {
+    light: 'gray.100',
+    dark: 'rgba(255,255,255,0.08)'
+  };
+
+  const topicsBgActive = {
+    light: '#ceedff',
+    dark: '#3a6aa8'
+  };
+
+  const topicsColor = {
+    light: 'black',
+    dark: 'white'
+  };
 
   const filteredBlogPosts = blogPosts
     .sort(
       (a, b) =>
         Number(new Date(b.publishedAt)) - Number(new Date(a.publishedAt))
     )
-    .filter((frontMatter) =>
-      frontMatter.title.toLowerCase().includes(searchValue.toLowerCase())
+    .filter(
+      (frontMatter) =>
+        selectedTopics.indexOf(frontMatter.category) !== -1 ||
+        selectedTopics.length < 1
+    )
+    .filter(
+      (frontMatter) =>
+        frontMatter.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+        frontMatter.summary.toLowerCase().includes(searchValue.toLowerCase())
     );
+
+  const topics = [...new Set(blogPosts.map((post) => post.category))];
+
+  const toggleSelectedTopics = (topic) => {
+    const topicIndex = selectedTopics.indexOf(topic);
+    topicIndex === -1
+      ? selectedTopics.push(topic)
+      : selectedTopics.splice(topicIndex, 1);
+    setSelectedTopics([...selectedTopics]);
+  };
 
   return (
     <>
@@ -66,6 +100,39 @@ const Blog = () => {
             untuk saya pribadi ketika sedang belajar atau membagikan sesuatu.
             Enjoy your reading!
           </Text>
+          <Flex
+            flexDirection="row"
+            flexWrap="wrap"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            mr={2}
+            w="100%"
+          >
+            {topics.map((topic, i) => {
+              let bgColor =
+                selectedTopics.indexOf(topic) === -1
+                  ? topicsBg[colorMode]
+                  : topicsBgActive[colorMode];
+              return (
+                <PseudoBox
+                  key={topic}
+                  as="button"
+                  p={2}
+                  m={1}
+                  rounded="md"
+                  fontSize="sm"
+                  bg={bgColor}
+                  color={topicsColor[colorMode]}
+                  _hover={{ bg: topicsBgActive[colorMode] }}
+                  _active={{ bg: topicsBgActive[colorMode] }}
+                  _selected={{ bg: topicsBgActive[colorMode] }}
+                  onClick={() => toggleSelectedTopics(topic)}
+                >
+                  {topic}
+                </PseudoBox>
+              );
+            })}
+          </Flex>
           <InputGroup my={4} mr={4} width="100%">
             <Input
               aria-label="Cari tulisan"
