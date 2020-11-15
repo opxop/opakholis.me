1332d91a9d8b18af3792e96323b18e7d0f2fdaec
Opa Kholis Majid <opakholis.id@gmail.com>
2020-11-15 21:26:45 +0700

    :bento: update icons

---
+++ styles/icons/nextjs.js
@@ -0,0 +1,11 @@
+import { createIcon } from '@chakra-ui/icons';
+
+export const NextJS = createIcon({
+  displayName: 'NextJS',
+  path: (
+    <g fill="currentColor">
+      <path d="M4.5 4.5l.405-.293A.5.5 0 0 0 4 4.5h.5zm3 9.5A6.5 6.5 0 0 1 1 7.5H0A7.5 7.5 0 0 0 7.5 15v-1zM14 7.5A6.5 6.5 0 0 1 7.5 14v1A7.5 7.5 0 0 0 15 7.5h-1zM7.5 1A6.5 6.5 0 0 1 14 7.5h1A7.5 7.5 0 0 0 7.5 0v1zm0-1A7.5 7.5 0 0 0 0 7.5h1A6.5 6.5 0 0 1 7.5 1V0zM5 12V4.5H4V12h1zm-.905-7.207l6.5 9l.81-.586l-6.5-9l-.81.586zM10 4v6h1V4h-1z" />
+    </g>
+  ),
+  viewBox: '0 0 15 15'
+});
