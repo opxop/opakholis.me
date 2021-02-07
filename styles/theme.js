693ab92f41d3cd1b6b340da05cf8812a02649472
Opa Kholis Majid <opakholis.id@gmail.com>
2021-02-07 15:15:06 +0700

    :adhesive_bandage: refactor: waiting for bugs fixed

--- styles/theme.js
+++ styles/theme.js
@@ -2,8 +2,8 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   config: {
-    // initialColorMode: 'dark',
-    useSystemColorMode: true
+    initialColorMode: 'dark',
+    useSystemColorMode: false
   },
 
   fonts: {
