2a4696c23f1e24bb006fcabc3b98aeb0571f6203
Opa Kholis Majid <opakholis.id@gmail.com>
2021-02-15 21:48:10 +0700

    :alien: refactor: something weired in about page

--- pages/about.js
+++ pages/about.js
@@ -28,34 +28,31 @@ const About = () => {
           <Box>
             <Text color={secondaryText}>Halo! Perkenalkan, saya Opa.</Text>
             <Text color={secondaryText} py={3}>
-              Nama yang unik, bukan? HaHaHa 😒
-              <br />
-              Saya adalah seorang Frontend developer — (anggap saja begitu) —
-              yang mana sekarang sedang senang ber-eksplorasi dengan Linux
-              distribution dan Open-source Software, ya meskipun sampai saat ini
-              belum pernah berkonstribusi didalamnya. ~hiks.
+              Saya adalah seorang Frontend developer (anggap saja begitu) yang
+              mana sekarang sedang senang ber-eksplorasi dengan Linux
+              distribution dan Open-source Software.
             </Text>
             <Text color={secondaryText} py={3}>
-              Terlepas dari ketertarikan pada OSS, kesibukan saya yang
-              sebenarnya adalah mengembangkan skill programming. Dengan
-              menggunakan stack dari bahasa javascript modern yaitu NodeJs dan
-              library React, kadang juga khilaf dengan berkecimpung pada mobile
-              development dengan menggunakan Framework Flutter, ~ehhe.
+              Terlepas dari ketertarikan pada OSS, kesibukan saya sebenarnya
+              adalah mengembangkan skill programming. Dengan menggunakan stack
+              dari bahasa javascript modern yaitu NodeJs dan library React,
+              kadang juga khilaf dengan berkecimpung pada mobile development
+              dengan menggunakan Framework Flutter, ~ehhe.
             </Text>
             <Text color={secondaryText} py={3}>
               Karena saya manusia sosial, jadi gak sepenuhnya saya mengahabiskan
               waktu di depan teks editor. Dan untuk mengistirahatkan otak, saya
-              meluangkan waktu untuk sekedar mendengarkan musik; menonton film,
-              terlebih jejepangan🥴; atau bahkan membaca buku-buku untuk
-              menambah insight baru(?). "sosial dimananya ha?"
+              meluangkan waktu untuk sekedar mendengarkan musik, menonton
+              film—terlebih jejepangan—atau bahkan membaca buku untuk menambah
+              insight baru(?). "sosial dimananya ha?"
             </Text>
 
             <Text color={secondaryText} py={3}>
-              Saya akui, saya gak begitu pandai dalam menulis. Ya, saya cupu.
-              Tapi saya ingin situs ini berguna, khususnya bagi saya pribadi
-              dengan mendokumentasikan segala sesuatu yang sedang dan/atau telah
-              saya pelajari — (semoga gak malas-malasan supaya kedepannya situs
-              ini gak gersang).
+              Dan dengan adanya situs ini, saya harap dapat menjadi acuan bagi
+              saya dalam pengembangan diri—menjadi lebih baik—dengan
+              mendokumentasikan segala sesuatu yang sedang dan/atau telah saya
+              pelajari (semoga gak malas-malasan supaya kedepannya situs ini
+              tetap hidup).
             </Text>
           </Box>
         </Box>
