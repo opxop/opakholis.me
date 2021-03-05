c47bafea662724ac75b9aa7bddb0a39ff26f366c
Restyled.io <commits@restyled.io>
2021-03-05 09:11:21 +0000

    Restyled by prettier

--- styles/theme.js
+++ styles/theme.js
@@ -1,17 +1,17 @@
-import {extendTheme} from '@chakra-ui/react';
+import { extendTheme } from '@chakra-ui/react';
 
 const systemFonts =
-    '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
 
 const theme = extendTheme({
-  config : {initialColorMode : 'dark', useSystemColorMode : false},
-  fonts : {
-    body : `Inter, ${systemFonts}`,
-    heading : `Inter, ${systemFonts}`,
-    mono :
-        'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace, monospace'
+  config: { initialColorMode: 'dark', useSystemColorMode: false },
+  fonts: {
+    body: `Inter, ${systemFonts}`,
+    heading: `Inter, ${systemFonts}`,
+    mono:
+      'Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace, monospace'
   },
-  fontWeights : {normal : 400, medium : 600, bold : 700}
+  fontWeights: { normal: 400, medium: 600, bold: 700 }
 });
 
 export default theme;
