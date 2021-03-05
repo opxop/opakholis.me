c373ee86cc2d38526aad0482d22095fff461a8ae
Opa Kholis Majid <opakholis.id@gmail.com>
2021-03-05 16:10:28 +0700

    :recycle: style: refactoring code

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,18 @@
-import { DefaultSeo } from 'next-seo';
 import { ChakraProvider, useColorMode } from '@chakra-ui/react';
 import { Global, css } from '@emotion/react';
+import { DefaultSeo } from 'next-seo';
 import NextNprogress from 'nextjs-progressbar';
 
 import SEO from '../next-seo.config';
+import Fonts from '@/styles/font-face';
 import themes from '@/styles/theme';
 import MDXComponents from '@/components/MDXComponents';
 import { MDXProvider } from '@mdx-js/react';
 import { prismDarkTheme, prismLightTheme } from '@/styles/prism';
 import 'focus-visible/dist/focus-visible';
-import Fonts from '@/styles/font-face';
 
 const GlobalStyle = ({ children }) => {
   const { colorMode } = useColorMode();
-
   return (
     <>
       <Global
