ede3ed1e75bbf20617b89763230e2f6df4509b3d
Opa Kholis Majid <opakholis.id@gmail.com>
2021-03-05 15:54:38 +0700

    :boom: feat: add contact page

---
+++ pages/contact.js
@@ -0,0 +1,41 @@
+import { NextSeo } from 'next-seo';
+import { Text, Heading, Box, useColorModeValue } from '@chakra-ui/react';
+
+import Container from '@/components/Container';
+import { CustomLink } from '@/components/MDXComponents';
+
+const url = 'https://opakholis.me/contact';
+const title = 'Mari Ngobrol! — Opa Kholis Majid';
+
+const About = () => {
+  const secondaryText = useColorModeValue('gray.700', 'gray.400');
+  return (
+    <>
+      <NextSeo title={title} canonical={url} openGraph={{ url, title }} />
+      <Container>
+        <Box pb={5} pt={2}>
+          <Heading as="h1" fontSize="5xl" letterSpacing="tight" my={5}>
+            Mari Ngobrol.
+          </Heading>
+          <Box>
+            <Text color={secondaryText} lineHeight="tall">
+              Saya punya akun Instagram, Twitter, Facebook, Discord, Slack,{' '}
+              <strike>Reddit</strike>, bahkan Goodreads sekalipun, tapi jarang
+              sekali dibuka. Hanya ketika senggang saja.
+            </Text>
+            <Text color={secondaryText} lineHeight="tall" my={4}>
+              Jika kamu ingin berkomunikasi dengan saya, saya cukup aktif di{' '}
+              <CustomLink href="https://t.me/hi_pluie/">Telegram</CustomLink>.
+              Bisa juga berkirim pesan lewat surat elektronik saya di{' '}
+              <CustomLink href="mailto:hi@opakholis.me">
+                hi@opakholis.me.
+              </CustomLink>
+            </Text>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default About;
