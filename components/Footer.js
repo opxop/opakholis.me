4f61872cea3f40ad71d3c12cf870ec3f36dd5e49
Opa Kholis Majid <opakholis.id@gmail.com>
2021-04-21 07:03:00 +0700

    :art: refactor(footer): improve link component

--- components/Footer.js
+++ components/Footer.js
@@ -1,48 +1,37 @@
 import {
   Box,
-  Flex,
   Link,
+  VStack,
   IconButton,
   useColorModeValue
 } from '@chakra-ui/react';
 
 import { MailIcon, GithubIcon, TwitterIcon } from '@/styles/icons';
 
-const Footer = () => {
+export default function Footer() {
   const secondaryText = useColorModeValue('gray.700', 'gray.400');
-  return (
-    <Box as="footer" mb={5} mt={2}>
-      <Flex justifyContent="center" alignItems="center">
-        <Link href="https://twitter.com/opakholis" isExternal>
-          <IconButton
-            aria-label="Github"
-            icon={<TwitterIcon boxSize={5} />}
-            size="lg"
-            color="gray.500"
-            variant="ghost"
-          />
-        </Link>
-        <Link href="https://github.com/opxop" isExternal>
-          <IconButton
-            aria-label="Github"
-            icon={<GithubIcon boxSize={5} />}
-            size="lg"
-            color="gray.500"
-            variant="ghost"
-          />
-        </Link>
-        <Link href="mailto:hi@opakholis.me" isExternal>
-          <IconButton
-            aria-label="Email"
-            icon={<MailIcon boxSize={5} />}
-            size="lg"
-            color="gray.500"
-            variant="ghost"
-          />
-        </Link>
-      </Flex>
 
-      <Flex justifyContent="center" alignItems="center">
+  return (
+    <VStack as="footer" mb={5} mt={2} spacing={0}>
+      <Box>
+        <Icon
+          href="https://twitter.com/opakholis"
+          ariaLabel="Twitter"
+          icon={<TwitterIcon boxSize={5} />}
+        />
+        <Icon
+          href="https://github.com/opxop"
+          ariaLabel="Github"
+          icon={<GithubIcon boxSize={5} />}
+        />
+        <Icon
+          href="mailto:hi@opakholis.me"
+          ariaLabel="Email"
+          icon={<MailIcon boxSize={5} />}
+        />
+      </Box>
+
+      <Box>
         <Link mx={2} color={secondaryText} href="/uses">
           /uses
         </Link>
@@ -52,9 +41,23 @@ const Footer = () => {
         <Link mx={2} color={secondaryText} href="/advice">
           /advice
         </Link>
-      </Flex>
-    </Box>
+      </Box>
+    </VStack>
+  );
+}
+
+const Icon = ({ href, ariaLabel, icon }) => {
+  return (
+    <IconButton
+      as="a"
+      href={href}
+      icon={icon}
+      aria-label={ariaLabel}
+      size="lg"
+      variant="ghost"
+      color="gray.500"
+      target="_blank"
+      rel="noreferrer noopener"
+    />
   );
 };
-
-export default Footer;
