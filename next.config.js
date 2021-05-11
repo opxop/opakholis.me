8dfee89d219c20e310134139bf404ab4d26e0501
Opa Kholis Majid <opakholis.id@gmail.com>
2021-05-11 22:27:20 +0700

    :sparkles: feat: pwa? why not!

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public'
+  },
   future: {
     webpack5: true
   },
@@ -12,4 +17,4 @@ module.exports = {
     }
     return config;
   }
-};
+});
