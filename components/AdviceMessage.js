7e508abd292ff5e50a8aed793574df228ad7723a
Opa Kholis Majid <opakholis.id@gmail.com>
2021-05-15 13:10:23 +0700

    :beers: refactor(advice): sort  advice message

--- components/AdviceMessage.js
+++ components/AdviceMessage.js
@@ -2,10 +2,14 @@ import { Box, Text } from '@chakra-ui/react';
 import { format, parseISO } from 'date-fns';
 
 export default function AdviceMessage({ advices }) {
+  const orderedAdvices = advices.sort(
+    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+  );
+
   return (
     <div>
-      {advices.map((advice) => (
-        <Box key={advice.createdAt} py={2}>
+      {orderedAdvices.map((advice, i) => (
+        <Box key={i} py={2}>
           <Text my={1}>{advice.text}</Text>
           <Text fontSize="small" color="gray.400">
             {format(parseISO(advice.createdAt), 'PP • p')}
