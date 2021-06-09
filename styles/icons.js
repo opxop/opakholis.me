5af7715c03430887145c0916a0c0eb07de56764c
Opa Kholis Majid <opakholis.id@gmail.com>
2021-06-09 16:36:06 +0700

    :bento: style: add trash icon

--- styles/icons.js
+++ styles/icons.js
@@ -191,3 +191,20 @@ export const DateIcon = createIcon({
   ),
   viewBox: '0 0 24 24'
 });
+
+export const TrashIcon = createIcon({
+  displayName: 'Trash',
+  path: (
+    <g
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="3 6 5 6 21 6"></polyline>
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+    </g>
+  ),
+  viewBox: '0 0 24 24'
+});
